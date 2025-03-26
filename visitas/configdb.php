3345208c07ba033c4efa73f8b059f0cf57504a55
AleGonzalezTrigo <147615266+AleGonzalezTrigo@users.noreply.github.com>
2025-03-26 09:06:18 +0100

    configdb.php

--- visitas/configdb.php
+++ visitas/configdb.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVIDOR",'1daw.esvirgua.com');
-define("USUARIO",'user1daw_14');
-define("PASSWORD",'GQnmf@d&eU_e');
-define("BBDD",'user1daw_BD1-14');
+define("SERVIDOR",'localhost');
+define("USUARIO",'root');
+define("PASSWORD",'');
+define("BBDD",'jesuita');
 ?>
